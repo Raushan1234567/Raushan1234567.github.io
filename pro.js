6adf317b4bf6c89d51c444a4270dd81232b2b6e8
Raushan1234567 <raushankumar241099@gmail.com>
2023-04-25 11:16:48 +0530

    raush

--- pro.js
+++ pro.js
@@ -3,10 +3,10 @@ let body = document.querySelector('body');
 toggle.addEventListener('click', function () {
     this.classList.toggle('bi-moon-fill');
     if(this.classList.toggle('bi-brightness-high')){
-        body.style.background = "#080808";
+        body.style.background = "#080809";
         body.style.transition = "2s";     
         document.getElementById('user-detail-name').style.color = "orangered";
-        document.getElementById('user-detail-name').style.transition = "2s";
+        document.getElementById('user-detail-name').style.transition = "1s";
         document.querySelector('#one>.text-1').style.color = "white";
         document.querySelector('#one>.text-1').style.transition = "2s";  
         document.querySelector('#one>.text-3>span').style.color = "white";
@@ -21,9 +21,9 @@ toggle.addEventListener('click', function () {
         document.querySelector('#skills>h1').style.color = "white";
         document.querySelector('#skills>h1').style.transition = "2s";
         document.querySelector('#projects>h1').style.color = "white";
-        document.querySelector('#projects>h1').style.transition = "2s";
+        document.querySelector('#projects>h1').style.transition = "1s";
         document.querySelector('.github-activity-calendar h1').style.color = "white";
-        document.querySelector('.github-activity-calendar h1').style.transition = "2s";
+        document.querySelector('.github-activity-calendar h1').style.transition = "1s";
        
     }else{
         body.style.background = "white";
@@ -62,9 +62,9 @@ function closemenu() {
 }
 // typing text animation script
 var typed = new Typed(".typing", {
-    strings: ["A Full W Stack Developer",  "A JAVA Backend Developer"],
-    typeSpeed: 100,
-    backSpeed: 60,
+    strings: ["A Full Stack W Developer",  "A JAVA Backend Developer"],
+    typeSpeed: 80,
+    backSpeed: 50,
     loop: true,
 })
 
@@ -89,35 +89,21 @@ const sr = ScrollReveal({
     reset: true
 })
 
-sr.reveal('#about h1', { delay: 200})
-sr.reveal('.home-img', { delay: 600 })
-sr.reveal('#user-detail-intro', { delay: 600 })
-sr.reveal('.skill', { delay: 200 })
+sr.reveal('#about h1', { delay: 100})
+sr.reveal('.home-img', { delay: 500 })
+sr.reveal('#user-detail-intro', { delay: 500 })
+sr.reveal('.skill', { delay: 100 })
 
 sr.reveal('.project', {})
 sr.reveal('.project-card img', { delay: 100 })
-sr.reveal('.project-title', { delay: 200 })
-sr.reveal('.project-description', { delay: 200 })
-sr.reveal('.project-tech-stack', { delay: 200 })
-sr.reveal('.repo-links', { delay: 200 })
+sr.reveal('.project-title', { delay: 100 })
+sr.reveal('.project-description', { delay: 100 })
+sr.reveal('.project-tech-stack', { delay: 100 })
+sr.reveal('.repo-links', { delay: 100 })
 
 sr.reveal('#github-streak-stats', { delay: 200 })
-sr.reveal('#github-top-langs', { delay: 400 })
-sr.reveal('#github-stats-card', { delay: 600 })
+sr.reveal('#github-top-langs', { delay: 300 })
+sr.reveal('#github-stats-card', { delay: 500 })
 
- sr.reveal('.calendar', { delay: 600 })
+ sr.reveal('.calendar', { delay: 500 })
   
- 
-
- 
-
- 
- 
-
- 
- 
- 
- 
- 
- 
-// </script>
